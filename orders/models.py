6fd27a0c6293459e50994a68b2f774e0c93d96af
Sushmitha G <sushmithagouda29@gmail.com>
2025-12-14 11:52:55 +0530

    63355

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class OrderSatus(models.Model):
+    name = models.CharField(max_length =50, unique=True)
+    def __str__(self):
+        return self.code
+class Order(models.Model):
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete = models.SET_NULL,
+        null =True,
+        blank =True
+    )
+    def __str__(self):
+        return self.code
