bdc1a73c016d9df21cc2078fa8c8132fc5cb0d9e
Sushmitha G <sushmithagouda29@gmail.com>
2025-12-14 20:03:11 +0530

    63766

--- orders/models.py
+++ orders/models.py
@@ -12,5 +12,14 @@ class Order(models.Model):
         null =True,
         blank =True
     )
+
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name='orders'
+    )
+
+    created_at =models.DateTimeField(auto_now_add =True)
+    total_price = models.DecimalField(max_digits =10,decimal_places=2)
     def __str__(self):
         return self.code
