e0bcb6b9fb104325008340ae6e735bdf1ec816d7
Sushmitha G <sushmithagouda29@gmail.com>
2025-12-14 13:48:59 +0530

    63767

--- home/models.py
+++ home/models.py
@@ -4,3 +4,12 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length = 100 , unique =True)
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length =100)
+    price = models.DecimalField(max_digits=8,decimal_places)
+    description = models.TextField()
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
